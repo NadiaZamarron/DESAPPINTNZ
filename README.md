66b48917b3c6567961be96fd459d4ba28e09f542
NadiaZamarron <ic.nadiagzc@hotmail.com>
2023-08-24 21:06:49 -0600

    Actividades 01 y 02

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 Nombre: Nadia Guadalupe Zamarrón Castrellón
 Correo: ic.nadiagzc@hotmail.com 
+
+# Actividad 01 - Programas hechos en clase
+p01-hola-mundo - Completada
+p02-paga-trabajador - Completada
+p02-venta-pizzas - Completada
+p04-procesa-calificaciones - Completada
+p05–tabla-de-conversion - Completada 
+p06-factorial-numeros - Completada 
---
+++ p02-paga-trabajador/Program.cs
@@ -0,0 +1,28 @@
+﻿//Nadia Zamarrón 
+//Calcula la paga de un trabajador, considerando las horas extra
+
+string nombre;
+int horas, hextra=0;
+float paga, pagaextra=0,pagabruta, paganeta, impuesto, tasa;
+
+Console.Clear();
+Console.WriteLine("Calculando la paga de un trabajador \n");
+Console.Write("Nombre ? ") ; nombre = Console.ReadLine();
+Console.Write("Horas trabajadas ? ") ; horas = int.Parse(Console.ReadLine());
+Console.Write("Paga por hora ? ") ; paga = float.Parse(Console.ReadLine());
+tasa = 0.03f;
+
+if(horas>40) {
+hextra = horas - 40;
+pagaextra = 
+pagabruta = 40 * paga + (2 * paga * hextra);
+} else
+pagabruta = horas * paga;
+impuesto = pagabruta * tasa;
+paganeta = pagabruta - impuesto;
+
+Console.WriteLine($"Resumen de pagos del trabajador {nombre}:\n");
+Console.WriteLine($"Horas trabajadas {horas} de las cuales {hextra} son extra, se asume una tasa de impuesto de {tasa:p2}");
+Console.WriteLine($"Paga bruta: {pagabruta,12:c2}");
+Console.WriteLine($"Impuessto: {impuesto,12:c2}");
+Console.WriteLine($"Paga neta: {paganeta,12:c2}");
