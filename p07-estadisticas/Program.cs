c1bff80e1a293f4e85934a9ccf15214935203158
NadiaZamarron <ic.nadiagzc@hotmail.com>
2023-09-01 10:39:40 -0600

    Actividad 3

---
+++ p07-estadisticas/Program.cs
@@ -0,0 +1,42 @@
+﻿// Calcula las estadisticas basicas de un arreglo de calificaciones 
+
+const int MAX=100;
+int n=0;
+double[] califs = null;
+
+double may=0, men=0, prom=0, var=0, desv=0;
+
+Console.Clear();
+Console.Write("Cuantos elementos vas a introducir?");
+n=int.Parse(Console.ReadLine());
+
+if(n>MAX){
+    Console.WriteLine("El maximo de elementos a introducir es {0}", MAX);
+    return 1;
+}
+
+califs=new double[n];
+for(int i=0; i<califs.Length; i++){
+    Console.Write($"califs[{i}]=");
+    califs[i]=double.Parse(Console.ReadLine());
+}
+
+est.Imprime(califs);
+may = est.Mayor(califs);
+men = est.Menor(califs);
+prom = est.Promedio(califs);
+var = est.Varianza(califs, prom);
+desv = Math.Sqrt(var);
+
+Console.WriteLine("\n Resumen de estadisticas: ");
+
+Console.WriteLine($"\n Numero de elementos: {califs.Length}");
+Console.WriteLine($"\n El mayor es: {may:f2}");
+Console.WriteLine($"\n El menor es: {men:f2} ");
+Console.WriteLine($"\n El promedio es: {prom:f2} ");
+Console.WriteLine($"\n La varianza es: {var:f2} ");
+Console.WriteLine($"\n La desviación es: {desv:f2} ");
+
+return 0;
+
+
---
+++ p07-estadisticas/est.cs
@@ -0,0 +1,32 @@
+// Biblioteca de funciones
+
+public static class est{
+    public static void Imprime(double[] a){
+        for(int i=0; i<a.Length; i++)
+            Console.WriteLine($"{a[i]:f2}");
+}
+public static double Mayor(double[] a){
+    double m=a[0];
+    foreach(double n in a)
+        if(n>m) m=n;
+    return m;
+}
+public static double Menor(double[] a) {
+    double m=a[0];
+    foreach(double n in a)
+        if(n<m) m=n;
+    return m;
+}
+public static double Promedio(double []a){
+    double p=0;
+    foreach (double n in a)
+      p+= n;
+    return p/ a.Length;
+}
+public static double Varianza (double []a, double p){
+    double s=0;
+    foreach(double n in a)
+        s += Math.Pow(n - p,2);
+    return s/ a.Length;
+}
+}
